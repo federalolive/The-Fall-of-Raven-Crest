198837e6dd9da1c61ce17c39a63695accf96239d
federalolive <j3pumphrey@gmail.com>
2020-11-20 09:12:54 -0600

    delete psuedocode not applicable to program

--- js/app.js
+++ js/app.js
@@ -1,71 +1,13 @@
-// WORK IN PROGRESS - SUBJECT TO CHANGE
-
-
-/*--------- Constants ----------*/
-// Classes
-    // Warrior
-        // healthpool
-        // starting items
-        // baseline dmg
-        // Abilities
-
-        
-    // Wizard
-        // healthpool
-        // starting items
-        // spells
-        // baseline dmg
-    // Ranger
-        // healthpool
-        // starting items
-        // baseline dmg
-        // Abilities
 const textElement = document.getElementById('text')
 const optionButtonsElement = document.getElementById('input-buttons')
-// Enemies
-    // Enemy - /Names/HP/skills/abilities (there wont be to many because I can see it become overwhelming)
-/*--------- Variables ----------*/
-// Turns (turn between player and AI)
-// Player Choices based upon encounter
 
 // This is the player's 'state' throughout the game and it represeants how the player may be affected as the user makes choices.
 let playerState = {}
 
-
-/*--------- Event Listeners ----------*/
-// 3-5 Buttons for User Input Choices
-// HTML/CSS Music & SoundFX Triggers
-// Dark/Light Mode Buttons
-
-
-
-
-
-/*--------- Cached Reference Elements ----------*/
-// MAIN MENU
-    // Start Button --> Pick Your Class Screen
-// Pick Your Class Screen
-    // 3 Buttons to Allows User to pick desired class (warrior, wizard, Ranger), 1 Input area allowing user to name their character, and Lastly 1 button for Start game which triggers the adventure to start. 
-
-// INGAME 
-    // User input buttons 
-
-// HTML/CSS Music & SoundFX 
-
-
-
-/*--------- Functions ----------*/
-// Abilities/Spells Functions - Calculating the functions that determine how much damage player deals AND Enemies deal
-// Saving throws
-
-// Calculating Damage Dealt
-// Calculating Damage Done
-// Getting additional items
-
 // Starts game and sets state as 
 function startGame () {
     state = {}
-    showTextNode = (0)
+    showTextNode = (1)
 
 }
 
@@ -75,8 +17,9 @@ function showTextNode(textNodeIndex) {
     while (optionButtonsElement.firstChild) {
       optionButtonsElement.removeChild(optionButtonsElement.firstChild)
     }
-}
-textNode.options.forEach(option => {
+
+ // Allows buttons to be customized so we do not always need to show 4 buttons
+ textNode.options.forEach(option => {
     if (showOption(option)) {
       const button = document.createElement('button')
       button.innerText = option.text
@@ -85,10 +28,20 @@ textNode.options.forEach(option => {
       optionButtonsElement.appendChild(button)
     }
   })
-
-function selectOption(option) {
-
 }
+
+function showOption(option) {
+    return option.requiredState == null || option.requiredState(state)
+  }
+
+  function selectOption(option) {
+    const nextTextNodeId = option.nextText
+    if (nextTextNodeId <= 0) {
+      return startGame()
+    }
+    state = Object.assign(state, option.setState)
+    showTextNode(nextTextNodeId)
+  }
 
 // Text nodes that drive story progression and subsequent choices.
 const textNodes = [
@@ -103,8 +56,8 @@ const textNodes = [
                 text: "Go search for an inn on your own",
             },
             {
-                text: "What's that voice that I keep hearing?",
-            },
+                text: "What!? Who said that!?!?",
+            }
         ]
     }
 ]
