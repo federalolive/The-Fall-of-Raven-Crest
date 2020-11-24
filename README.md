c54ae4b4ce6d1ede81722c78a1c3efcf511d0339
federalolive <j3pumphrey@gmail.com>
2020-11-24 10:18:55 -0600

    Updated README

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# Project-Cultist - 
+# The Fall of Recen Crest
 
-https://unequaled-stretch.surge.sh/
+TFRC is an adventure like novel of my own imagination. Taking inspiration from fantesy genres and popular games such as *Dungeons and Dragons* and *World of Warcraft* in it you follow the narrative in effort to reach the end of the story.
 
-## Multi User Dungeon Text based RPG
+## Installation
+
+The fall of Raven Crest can be accessed through [My GitHub](https://github.com/federalolive/Unit-1-Project.git) or through [Surge](https://unequaled-stretch.surge.sh/).
+
+### Conceptual Wireframes
 
 <img title ='Main Menus' src="https://i.imgur.com/FYHMFo1.png">
 
@@ -10,3 +14,41 @@ https://unequaled-stretch.surge.sh/
 
 <img title ='Game Screen' src="https://i.imgur.com/PSUwWid.png">
 
+### Final Outcome
+
+<img title ='Final Outcome' src="https://i.imgur.com/MnT2FvX.png">
+
+## Features
+*The fall Of Raven Crest features multiple ways of navigating through the narrative that I created, however there are some choices that are dead ends and will restart the story.
+
+*Written in HTML/CSS/Javascript
+
+*Dynamic player state that effects choices and outcomes
+
+# Planned Future Release
+Due to time constraints, as well other extenuating factors there were some features that I was not able to implement in this iteration of the program that will be added at a later time. These features include:
+* Additional dialogue and User choices.
+* Party System.
+  * Allowing for AI generated companions or player controlled (likely to be much further down the road).
+* Combat and Inventory systems.
+* Class system.
+  * Allow players to pick a "class" given them access to abilities both in and outside of combat that drive story and narrative.
+* Sound effects/Music to provide queues and ambience.
+* Fixes to animation.
+  * Resetting div class on each button click to allow for scrolling text animation.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## Credits
+Assets used in my program are open sourced and credit given to authors below.
+alphardex - for creating the animation text effects and allowing it to be open sourced.
+
+Kirill - for your particle fire animation
+
+## Special Thanks
+Tyler and Patrick - for trying to help me get the animation to work as intended. 
+
+Shahzad, Ben, David (no particular order) - for giving me direction and providing perspective on scope of what could be achieved vs what might could be beyond my ability to complete for this project given the timeframe that I had, and lastly for critiquing my code.
--- js/app.js
+++ js/app.js
@@ -53,10 +53,10 @@ function showOption(option) {
 const textNodes = [
     {
         id: 0,
-        text: "After a long and arduous journey from the capital, you reach your destination of Raven's Crest just as the sun begins to set. Raven's Crest is a quiet town that resides near The Duskwood and the Wildlands. You walk into town and you notice the townsfolk look at you with steely cold eyes and glancing stares. You disregard it as you have experianced this treatment before and there are other matters to tend to.",
+        text: "After a long and arduous journey from the capital, you reach your destination of Raven's Crest just as the sun begins to set. Raven's Crest is a quiet town that resides near The Duskwood and the Wildlands. You walk into town and you notice the townsfolk look at you with steely cold eyes and glancing stares. You disregard it as you have experienced this treatment before and there are other matters to tend to.",
         options: [
             {
-                text: "Ask one of the viligers where you can find a room and an ale",
+                text: "Ask one of the villagers where you can find a room and an ale",
                 nextText: 1
             },
             {
@@ -76,7 +76,7 @@ const textNodes = [
     },
     {
         id: 1,
-        text: "As you are walking through the streets, you come accross a woman and ask her directions. \"Piss off ya vagrant, we wont nothing to do with you outsiders\" she excalims.",
+        text: "As you are walking through the streets, you come accross a woman and ask her directions. \"Piss off ya vagrant, we wont nothing to do with you outsiders\" she exclaims.",
         options: [
             {
                 text: "\"I beg your pardon\" You apologize to the woman and depart to find the inn on your own.",
@@ -90,7 +90,7 @@ const textNodes = [
     },
     {
         id: 2,
-        text: "You head off in search of the inn, and after a short while you come accross \"The Rabid Weasal\". You mutter to yourself about the ridiculousness of such a name and contemplate how even such a name came to be, but just as quickly drop it as the desire to have a cold drink, hot food, and later a warm bed outweigh the thoughts of a silly name for an inn.",
+        text: "You head off in search of the inn, and after a short while you come across \"The Rabid Weasel\". You mutter to yourself about the ridiculousness of such a name and contemplate how even such a name came to be, but just as quickly drop it as the desire to have a cold drink, hot food, and later a warm bed outweigh the thoughts of a silly name for an inn.",
         options: [
             {
                 text: "Enter the tavern",
@@ -114,25 +114,25 @@ const textNodes = [
     },
     {
         id: 4,
-        text: "\"Hello?\" \"Who said that?\" you cried out nervously. \"How did you know I came from the capital? and where are you speaking to me from?\" you called out even louder. Disregarding your perception to the villigers you continuously seek to find asnwers to the source of the voices and disregard the small crowd that begins to form around you. You hear one vilager cry out \"Oi! He's talkin' to 'imself and mutterin' bout voices in 'is head!\" Other vilagers call out their criticisms of your behavior, and your heart sinks as you realize that you have drawn too much unwanted attention to yourself.",
+        text: "\"Hello?\" \"Who said that?\" you cried out nervously. \"How did you know I came from the capital? and where are you speaking to me from?\" you called out even louder. Disregarding your perception to the villagers you continuously seek to find answers to the source of the voices and disregard the small crowd that begins to form around you. You hear one villager cry out \"Oi! He's talkin' to 'imself and muttering' bout voices in 'is head!\" Other villagers call out their criticisms of your behavior, and your heart sinks as you realize that you have drawn too much unwanted attention to yourself.",
         options: [
             {
                 text: "Down play the situation as if it were a joke.",
                 nextText: 8
             },
             {
-                text: "Apologize for the specticle as it has been a really long and tiresome journey and you are fatigued.",
+                text: "Apologize for the spectacle as it has been a really long and tiresome journey and you are fatigued.",
                 nextText: 9
             },
             {
-                text: "\"Project\" your craziness onto someone in the croud to try and shake off the croud.",
+                text: "\"Project\" your craziness onto someone in the crowd to try and shake off the crowd.",
                 nextText: 10
             }
         ]
     },
     {
         id: 5,
-        text: "You shout as you strike the woman as hard as you can. The force of your blow knocking her back and down to the ground. You quickly realize that you two are not alone as you hear someone shout \"Guards! Guards!\" and before you can get away you here the sound of armored footsteps draw near you from behind, followed by the sensations of speartips digging through into your tunic enough to get your attention.",
+        text: "You shout as you strike the woman as hard as you can. The force of your blow knocking her back and down to the ground. You quickly realize that you two are not alone as you hear someone shout \"Guards! Guards!\" and before you can get away you hear the sound of armored footsteps draw near you from behind, followed by the sensations of spear-tips digging through into your tunic enough to get your attention.",
         options: [
             {
                 text: "Fight your way out of the situation.",
@@ -146,7 +146,7 @@ const textNodes = [
     },
     {
         id: 6,
-        text: "As you approach the inn, a sudden chill pierces you and dances on your spine, the night is settling in. You notice a fog begin to rise and you feel a sense of joy and gratitude that you will not be spending your time in it. You turn your attention to the inn itself and notice that the outside of it looks gloomy, dull and dire. Stacked stones and marble details make up most of the building's outer structure. It's difficult to see through the small, stained glass windows, but the ominous atmosphere from within can be felt outside. As you enter the tavern through the thick, wooden door, you're welcomed by thick air and a feeling of discomfort. The bartender is talking to a customer and makes no effort to acknowledge your pressence. It's as somber inside as it is on the outside. Tree logs support the upper floor and the unlit torches attached to them. The walls are scattered with decor and game that has been hunted by locals. The inn is moderatly occupied with vilagers and people alike, all occupying their tables and tending to their conversations\,\ drink\,\ and food alike. You did hear rumors about this tavern, supposedly it's infamous for something, but for the life of you you can't remember what for.", 
+        text: "As you approach the inn, a sudden chill pierces you and dances on your spine, the night is settling in. You notice a fog begin to rise and you feel a sense of joy and gratitude that you will not be spending your time in it. You turn your attention to the inn itself and notice that the outside of it looks gloomy, dull and dire. Stacked stones and marble details make up most of the building's outer structure. It's difficult to see through the small, stained glass windows, but the ominous atmosphere from within can be felt outside. As you enter the tavern through the thick, wooden door, you're welcomed by thick air and a feeling of discomfort. The bartender is talking to a customer and makes no effort to acknowledge your presence. It's as somber inside as it is on the outside. Tree logs support the upper floor and the unlit torches attached to them. The walls are scattered with decor and game that has been hunted by locals. The inn is moderately occupied with villagers and people alike, all occupying their tables and tending to their conversations\,\ drink\,\ and food alike. You did hear rumors about this tavern, supposedly it's infamous for something, but for the life of you you can't remember what for.", 
         options: [
             {
                 text: "Walk over to the innkeeper.",
@@ -156,7 +156,7 @@ const textNodes = [
     },
     {
         id: 7,
-        text: "You notice that you are alone and it is errily quiet. You have been through plenty of towns and villages before at all times of day, but have yet to experiance a stillness such as this. You dismiss such feelings and collect yourself as you walk up to the inn.",
+        text: "You notice that you are alone and it is eerily quiet. You have been through plenty of towns and villages before at all times of day, but have yet to experience a stillness such as this. You dismiss such feelings and collect yourself as you walk up to the inn.",
         options: [
             {
                 text: "Enter the inn.",
@@ -166,10 +166,10 @@ const textNodes = [
     },
     {
         id: 8,
-        text: "You nervously laugh in an attempt to brush off the experiance as being nothing more than a joke. The viligers look at you with the least bit of amusement. \"This one's another weird one!\" one viligers called out, \"Let's get rid of 'em!\" cried out another, \"I bet they're responsible for all of the peculiar things that have befalling our town!\" screached another woman. The viligers began to qaurrel amongst themsevles, and you mumble under your breath in annoyance this voice has caused. \"You're not crazy untill you start replying\"....",
+        text: "You nervously laugh in an attempt to brush off the experience as being nothing more than a joke. The villagers look at you with the least bit of amusement. \"This one's another weird one!\" one villagers called out, \"Let's get rid of 'em!\" cried out another, \"I bet they're responsible for all of the peculiar things that have befalling our town!\" screeched another woman. The villagers began to quarrel amongst themselves, and you mumble under your breath in annoyance this voice has caused. \"You're not crazy until you start replying\"....",
         options: [
             {
-                text: "Wait patiently for the viligers to decide what to do next.",
+                text: "Wait patiently for the villagers to decide what to do next.",
                 nextText: 14
             },
             {
@@ -188,7 +188,7 @@ const textNodes = [
     },
     {
         id: 9,
-        text: "You vehemently apologize for creating the specticle, expressing that you have been traveling for so long and that you are fatigied from hunger and thirst, and as a result lost bearing with your sense for an all to brief moment. This of course being a lie, you are very well of the voice that you continue to hear that continuously is narrating, but you are also smart enough to down play it. The crowd looks at you with a mixture of contempt and pitty as you sense that some are angry with you and others are not convinced that you are not an imbecile. You don't care however, as the disperse allowing you to go along your way. You decide for yourself that you have had enough of today and make your way to the inn. \"Shut up voice\" you mutter underyour breath.",
+        text: "You vehemently apologize for creating the spectacle, expressing that you have been traveling for so long and that you are fatigued from hunger and thirst, and as a result lost bearing with your sense for an all to brief moment. This of course being a lie, you are very well of the voice that you continue to hear that continuously is narrating, but you are also smart enough to down play it. The crowd looks at you with a mixture of contempt and pity as you sense that some are angry with you and others are not convinced that you are not an imbecile. You don't care however, as the disperse allowing you to go along your way. You decide for yourself that you have had enough of today and make your way to the inn. \"Shut up voice\" you mutter under your breath.",
         options: [
             {
                 text: "Head to the inn",
@@ -198,7 +198,7 @@ const textNodes = [
     },
     {
         id: 10,
-        text: "You frantically look around the crowd, looking for the biggest idiot you can. Your eyes rest upon a large heavyset man with cross eyes and patched hair. Before you can even think this plan through the words escape your mouth, \"I'm not crazy!! He is crazy!\" as you point your finger at your target in a clear projection. In perfect annunciation the man defends himself, denying such accusations as having no base. \"Lets put 'em in the stocks! See if spending the night outside will clear his head\" a few viligers cry out in unison. Before you can react you are grabbed by several people and they begin to carry you off.", 
+        text: "You frantically look around the crowd, looking for the biggest idiot you can. Your eyes rest upon a large heavyset man with cross eyes and patched hair. Before you can even think this plan through the words escape your mouth, \"I'm not crazy!! He is crazy!\" as you point your finger at your target in a clear projection. In perfect annunciation the man defends himself, denying such accusations as having no base. \"Lets put 'em in the stocks! See if spending the night outside will clear his head\" a few villagers cry out in unison. Before you can react you are grabbed by several people and they begin to carry you off.", 
         options: [
             {
                 text: "Try to escape",
